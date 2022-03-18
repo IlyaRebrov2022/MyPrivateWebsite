2e975a3a5a7a01cebf513a308a94b3335e98c75c
IlyaRebrov2022 <ilyarebrovkripta@gmail.com>
2022-03-18 16:33:32 +0200

    Delete buttonUp

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,11 @@
 import React from "react";
 import s from './Footer.module.css'
-import ButtonUp from "./ButtonUp/ButtonUp";
 
 
 const Footer = () => {
     return (
         <div className={s.footer}>
             Footer
-            <ButtonUp/>
         </div>
     )
 }
